bcaaea17bc0926409e53663273fade5991dd345b
jayeen28 <jayeen6783@gmail.com>
2022-08-18 00:42:06 +0600

    cleanup

--- manageCounters.js
+++ manageCounters.js
@@ -1,3 +1,19 @@
+const counterBox = (count, key) => `
+    <div class="max-w-md mx-auto mt-10 space-y-5">
+    <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+        <div class="text-2xl font-semibold" id="counter">${count}</div>
+        <div class="flex space-x-3">
+            <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" title="${key}" onclick="increment(event)">
+                Increment
+            </button>
+            <button class="bg-red-400 text-white px-3 py-2 rounded shadow" title="${key}" onclick="decrement(event)">
+                Decrement
+            </button>
+        </div>
+        </div>
+    </div>
+`;
+// Shotcut to get an element by Id.
 const getElemId = id => document.getElementById(id);
 
 // Elements
@@ -5,6 +21,7 @@ const countersWrapper = getElemId('counters-wrapper');
 const addCounter = getElemId('addCounter');
 const resetBtn = getElemId('reset');
 
+// The initial state
 const initialState = {
     counters: {
         counter1: 0
@@ -78,26 +95,11 @@ const manageRender = () => {
     countersWrapper.innerHTML = "";
     Object.keys(state.counters).forEach(key => {
         const elem = document.createElement('div');
-        elem.innerHTML = `
-        <div class="max-w-md mx-auto mt-10 space-y-5">
-         <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-             <div class="text-2xl font-semibold" id="counter">${state.counters[key]}</div>
-             <div class="flex space-x-3">
-                 <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="${key}" onclick="increment(event)">
-                     Increment
-                 </button>
-                 <button class="bg-red-400 text-white px-3 py-2 rounded shadow" id="${key}" onclick="decrement(event)">
-                     Decrement
-                 </button>
-             </div>
-            </div>
-        </div>
-        `;
-        countersWrapper.appendChild(elem)
+        elem.innerHTML = counterBox(state.counters[key], key);
+        countersWrapper.appendChild(elem);
     });
 
-}
-
+};
 manageRender();
 
 // Subscribe to store for getting the state updates
@@ -108,7 +110,7 @@ addCounter.addEventListener('click', () => {
     const length = Object.keys(store.getState().counters).length;
     store.dispatch(actionPayloads.addCounter(length))
 });
-const increment = e => store.dispatch(actionPayloads.increment(e.target.id, 5));
-const decrement = e => store.dispatch(store.dispatch(actionPayloads.decrement(e.target.id, 5)));
+const increment = e => store.dispatch(actionPayloads.increment(e.target.title, 5));
+const decrement = e => store.dispatch(actionPayloads.decrement(e.target.title, 5));
 resetBtn.addEventListener('click', () => store.dispatch(actionPayloads.reset()))
 //
